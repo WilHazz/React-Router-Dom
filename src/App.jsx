53c30b48b74f95aee4ec11136be0baaac45867de
Hanny Paz <hwpazz@gmail.com>
2025-04-30 13:23:41 -0500

    se agrega algunas tablas y las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-export const App = () => {
+import { Navigate, Route, Routes } from "react-router";
+import { NavBar } from "./components/NavBar";
+import { HomeScreen } from "./routers/HomeScreen";
+import { AboutScreen } from "./routers/AboutScreen";
+import { ContactScreen } from "./routers/ContactScreen";
+
+const App = () => {
   return (
     <>
-      <h1>Aplicacion de enrutamiento</h1>
-      <hr />
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<HomeScreen></HomeScreen>}></Route>
+        <Route path="About" element={<AboutScreen></AboutScreen>}></Route>
+        <Route path="Contact" element={<ContactScreen></ContactScreen>}></Route>
+        <Route path="/*" element={<Navigate to="/" />}></Route>
+      </Routes>
     </>
   );
 };
+
+export default App;
